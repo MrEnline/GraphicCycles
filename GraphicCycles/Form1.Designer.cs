c738004d1d649e498f620ce697733efde664bb81
Sergey Kuznetsov <mr.enline@gmail.com>
2019-01-09 07:35:27 +0300

    Первый коммит

---
+++ GraphicCycles/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace GraphicCycles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonLines = new System.Windows.Forms.Button();
+            this.buttonAllLines = new System.Windows.Forms.Button();
+            this.buttonDiagLines = new System.Windows.Forms.Button();
+            this.buttonConeOfRays = new System.Windows.Forms.Button();
+            this.buttonConeOfRays1 = new System.Windows.Forms.Button();
+            this.buttonSquares = new System.Windows.Forms.Button();
+            this.buttonSquares1 = new System.Windows.Forms.Button();
+            this.buttonTracery = new System.Windows.Forms.Button();
+            this.buttonTracery1 = new System.Windows.Forms.Button();
+            this.buttonLinesInSquares = new System.Windows.Forms.Button();
+            this.buttonCirclesInSquares = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 168);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(301, 301);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(249, 23);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(24, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(31, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonLines
+            // 
+            this.buttonLines.Location = new System.Drawing.Point(61, 23);
+            this.buttonLines.Name = "buttonLines";
+            this.buttonLines.Size = new System.Drawing.Size(42, 23);
+            this.buttonLines.TabIndex = 3;
+            this.buttonLines.Text = "Lines";
+            this.buttonLines.UseVisualStyleBackColor = true;
+            this.buttonLines.Click += new System.EventHandler(this.buttonLines_Click);
+            // 
+            // buttonAllLines
+            // 
+            this.buttonAllLines.Location = new System.Drawing.Point(109, 23);
+            this.buttonAllLines.Name = "buttonAllLines";
+            this.buttonAllLines.Size = new System.Drawing.Size(52, 23);
+            this.buttonAllLines.TabIndex = 4;
+            this.buttonAllLines.Text = "AllLines";
+            this.buttonAllLines.UseVisualStyleBackColor = true;
+            this.buttonAllLines.Click += new System.EventHandler(this.buttonAllLines_Click);
+            // 
+            // buttonDiagLines
+            // 
+            this.buttonDiagLines.Location = new System.Drawing.Point(167, 23);
+            this.buttonDiagLines.Name = "buttonDiagLines";
+            this.buttonDiagLines.Size = new System.Drawing.Size(63, 23);
+            this.buttonDiagLines.TabIndex = 5;
+            this.buttonDiagLines.Text = "DiagLines";
+            this.buttonDiagLines.UseVisualStyleBackColor = true;
+            this.buttonDiagLines.Click += new System.EventHandler(this.buttonDiagLines_Click);
+            // 
+            // buttonConeOfRays
+            // 
+            this.buttonConeOfRays.Location = new System.Drawing.Point(61, 52);
+            this.buttonConeOfRays.Name = "buttonConeOfRays";
+            this.buttonConeOfRays.Size = new System.Drawing.Size(80, 23);
+            this.buttonConeOfRays.TabIndex = 6;
+            this.buttonConeOfRays.Text = "ConeOfRays";
+            this.buttonConeOfRays.UseVisualStyleBackColor = true;
+            this.buttonConeOfRays.Click += new System.EventHandler(this.buttonConeOfRays_Click);
+            // 
+            // buttonConeOfRays1
+            // 
+            this.buttonConeOfRays1.Location = new System.Drawing.Point(150, 52);
+            this.buttonConeOfRays1.Name = "buttonConeOfRays1";
+            this.buttonConeOfRays1.Size = new System.Drawing.Size(90, 23);
+            this.buttonConeOfRays1.TabIndex = 7;
+            this.buttonConeOfRays1.Text = "ConeOfRays1";
+            this.buttonConeOfRays1.UseVisualStyleBackColor = true;
+            this.buttonConeOfRays1.Click += new System.EventHandler(this.buttonConeOfRays1_Click);
+            // 
+            // buttonSquares
+            // 
+            this.buttonSquares.Location = new System.Drawing.Point(23, 81);
+            this.buttonSquares.Name = "buttonSquares";
+            this.buttonSquares.Size = new System.Drawing.Size(55, 23);
+            this.buttonSquares.TabIndex = 8;
+            this.buttonSquares.Text = "Squares";
+            this.buttonSquares.UseVisualStyleBackColor = true;
+            this.buttonSquares.Click += new System.EventHandler(this.buttonSquares_Click);
+            // 
+            // buttonSquares1
+            // 
+            this.buttonSquares1.Location = new System.Drawing.Point(84, 81);
+            this.buttonSquares1.Name = "buttonSquares1";
+            this.buttonSquares1.Size = new System.Drawing.Size(64, 23);
+            this.buttonSquares1.TabIndex = 9;
+            this.buttonSquares1.Text = "Squares1";
+            this.buttonSquares1.UseVisualStyleBackColor = true;
+            this.buttonSquares1.Click += new System.EventHandler(this.buttonSquares1_Click);
+            // 
+            // buttonTracery
+            // 
+            this.buttonTracery.Location = new System.Drawing.Point(154, 81);
+            this.buttonTracery.Name = "buttonTracery";
+            this.buttonTracery.Size = new System.Drawing.Size(64, 23);
+            this.buttonTracery.TabIndex = 10;
+            this.buttonTracery.Text = "Tracery";
+            this.buttonTracery.UseVisualStyleBackColor = true;
+            this.buttonTracery.Click += new System.EventHandler(this.buttonTracery_Click);
+            // 
+            // buttonTracery1
+            // 
+            this.buttonTracery1.Location = new System.Drawing.Point(224, 81);
+            this.buttonTracery1.Name = "buttonTracery1";
+            this.buttonTracery1.Size = new System.Drawing.Size(64, 23);
+            this.buttonTracery1.TabIndex = 11;
+            this.buttonTracery1.Text = "Tracery1";
+            this.buttonTracery1.UseVisualStyleBackColor = true;
+            this.buttonTracery1.Click += new System.EventHandler(this.buttonTracery1_Click);
+            // 
+            // buttonLinesInSquares
+            // 
+            this.buttonLinesInSquares.Location = new System.Drawing.Point(154, 110);
+            this.buttonLinesInSquares.Name = "buttonLinesInSquares";
+            this.buttonLinesInSquares.Size = new System.Drawing.Size(94, 23);
+            this.buttonLinesInSquares.TabIndex = 12;
+            this.buttonLinesInSquares.Text = "LinesInSquares";
+            this.buttonLinesInSquares.UseVisualStyleBackColor = true;
+            this.buttonLinesInSquares.Click += new System.EventHandler(this.buttonLinesInSquares_Click);
+            // 
+            // buttonCirclesInSquares
+            // 
+            this.buttonCirclesInSquares.Location = new System.Drawing.Point(154, 139);
+            this.buttonCirclesInSquares.Name = "buttonCirclesInSquares";
+            this.buttonCirclesInSquares.Size = new System.Drawing.Size(94, 23);
+            this.buttonCirclesInSquares.TabIndex = 13;
+            this.buttonCirclesInSquares.Text = "CirclesInSquares";
+            this.buttonCirclesInSquares.UseVisualStyleBackColor = true;
+            this.buttonCirclesInSquares.Click += new System.EventHandler(this.buttonCirclesInSquares_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 471);
+            this.Controls.Add(this.buttonCirclesInSquares);
+            this.Controls.Add(this.buttonLinesInSquares);
+            this.Controls.Add(this.buttonTracery1);
+            this.Controls.Add(this.buttonTracery);
+            this.Controls.Add(this.buttonSquares1);
+            this.Controls.Add(this.buttonSquares);
+            this.Controls.Add(this.buttonConeOfRays1);
+            this.Controls.Add(this.buttonConeOfRays);
+            this.Controls.Add(this.buttonDiagLines);
+            this.Controls.Add(this.buttonAllLines);
+            this.Controls.Add(this.buttonLines);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Графические циклы";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonLines;
+        private System.Windows.Forms.Button buttonAllLines;
+        private System.Windows.Forms.Button buttonDiagLines;
+        private System.Windows.Forms.Button buttonConeOfRays;
+        private System.Windows.Forms.Button buttonConeOfRays1;
+        private System.Windows.Forms.Button buttonSquares;
+        private System.Windows.Forms.Button buttonSquares1;
+        private System.Windows.Forms.Button buttonTracery;
+        private System.Windows.Forms.Button buttonTracery1;
+        private System.Windows.Forms.Button buttonLinesInSquares;
+        private System.Windows.Forms.Button buttonCirclesInSquares;
+    }
+}
+
---
+++ GraphicCycles/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicCycles
+{
+    public partial class Form1 : Form
+    {
+        Graphics graphics;
+        Pen pen;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitGraph();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void InitGraph()
+        {
+            Bitmap bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bitmap;
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            pen = new Pen(Color.DarkRed);
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        public void Clear()
+        {
+            graphics.Clear(pictureBox1.BackColor);
+            Refresh();
+        }
+
+        public void Sleep(int time)
+        {
+            Refresh();
+            Thread.Sleep(time);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            graphics.DrawLine(pen, 0, 0, 300, 300);
+            graphics.DrawLine(pen, 300, 0, 0, 300);
+            Refresh();
+        }
+
+        public void HorizontalLines()
+        {
+            for (int y = 0; y <= 300; y+=30)
+            {
+                graphics.DrawLine(pen, 0, y, 300, y);
+                Sleep(100);
+            }
+            Refresh();
+        }
+
+        public void VerticallLines()
+        {
+            for (int x = 0; x <= 300; x += 30)
+            {
+                graphics.DrawLine(pen, x, 0, x, 300);
+                Sleep(100);
+            }
+            Refresh();
+        }
+
+        public void AllLines()
+        {
+            for (int x = 0; x <= 300; x += 30)
+            {
+                graphics.DrawLine(pen, x, 0, x, 300);
+                int y = x;
+                graphics.DrawLine(pen, 0, y, 300, y);
+                Sleep(100);
+            }
+            Refresh();
+        }
+
+        public void DiagLines()
+        {
+
+            //либо 2 цикла сделать
+            for (int r = 0; r < 300; r+=30)
+            {
+                graphics.DrawLine(pen, 0, r, r, 0);
+                //graphics.DrawLine(pen, 300, r, r, 300);
+                graphics.DrawLine(pen, r, 300, 300, r);
+                Sleep(200);
+            }
+            Refresh();
+        }
+
+        public void ConeOfRays()
+        {
+            for (int y = 0; y <= 300; y+=10)
+                graphics.DrawLine(pen, 0, y, 300, 0);
+            for (int y = 0; y <= 300; y += 10)
+                graphics.DrawLine(pen, 0, 300, 300, y);
+            Refresh();
+        }
+
+        public void ConeOfRays1()
+        {
+            for (int z = 0; z <= 300; z+=10)
+            {
+                graphics.DrawLine(pen, 0, 300, z, 0);
+                graphics.DrawLine(pen, 0, 300, 300, z);
+                Sleep(100);
+            }
+
+            for (int z = 0; z <= 300; z += 10)
+            {
+                graphics.DrawLine(pen, 300, 0, 0, z);
+                graphics.DrawLine(pen, 300, 0, z, 300);
+                Sleep(100);
+            }
+
+            for (int z = 0; z <= 300; z+=10)
+            {
+                graphics.DrawLine(pen, 0, 0, 300, z);
+                graphics.DrawLine(pen, 0, 0, z, 300);
+                Sleep(100);
+            }
+
+            for (int z = 300; z >= 0; z-=10)
+            {
+                graphics.DrawLine(pen, 300, 300, 0, z);
+                graphics.DrawLine(pen, 300, 300, z, 0);
+                Sleep(100);
+            }                     
+            Refresh();
+        }
+
+        public void Squares()
+        {
+
+            for (int y = 0; y < 300; y+=30)
+            {
+                for (int x = 0; x < 300; x += 30)
+                {
+                    graphics.DrawRectangle(pen, x + 3, y + 3, 24, 24);
+                    Sleep(20);
+                }
+            }
+            Refresh();
+        }
+
+        public void Squares1()
+        {
+
+            for (int y = 0; y < 300; y += 30)
+            {
+                for (int x = 0; x < 300; x += 30)
+                {
+                    if (x == 0 || x == 270 || y == 0 || y == 270)
+                    {
+                        graphics.DrawRectangle(pen, x + 3, y + 3, 24, 24);
+                        Sleep(20);
+                    }
+                }
+            }
+            Refresh();
+        }
+
+        public void CreateTracery()
+        {
+            for (int z = 0; z <= 300; z+=30)
+            {
+                if ((z == 120 || z == 150))
+                    continue;
+                graphics.DrawRectangle(pen, z + 3, z + 3, 24, 24);
+                graphics.DrawRectangle(pen, 270 - z + 3, z + 3, 24, 24);
+                graphics.DrawRectangle(pen, z+3, 153, 24, 24);
+                graphics.DrawRectangle(pen, z+3, 123, 24, 24);
+                graphics.DrawRectangle(pen, 153, z + 3, 24, 24);
+                graphics.DrawRectangle(pen, 123, z + 3, 24, 24);
+                Sleep(100);
+            }
+            Refresh();
+        }
+
+        public void CreateTracery1()
+        {
+            for (int y = 0; y < 300; y += 30)
+            {
+                for (int x = 0; x < 300; x += 30)
+                {
+                    if ((x == 120 || x == 150) && (y == 120 || y == 150))
+                        continue;
+                    if (x == y || 270 - x == y || x == 150 || x == 120 || y == 120 || y == 150)
+                    {
+                        graphics.DrawRectangle(pen, x + 3, y + 3, 24, 24);
+                        Sleep(20);
+                    }
+                }
+            }
+            Refresh();
+        }
+
+        public void LinesInSquares()
+        {
+
+            for (int y = 0; y < 300; y += 30)
+            {
+                for (int x = 0; x < 300; x += 30)
+                {
+                    graphics.DrawRectangle(pen, x + 3, y + 3, 24, 24);
+                    Sleep(10);
+                    //интересный узор
+                    //for (int z = 0; z < 24; z+=3)
+                    //{
+                    //    graphics.DrawLine(pen, x + 3 + z, y + 3, x + 3, y + 3 + z);
+                    //}
+
+                    for (int z = 0; z < 24; z += 3)
+                    {
+                        graphics.DrawLine(pen, x + 3 + z, y + 3, x + 3 + z, y + 27);
+                        graphics.DrawLine(pen, x + 3, y + 3 + z, x + 27, y + 3 + z);
+                        Sleep(3);
+                    }
+                }
+            }
+            Refresh();
+        }
+
+        public void CirclesInSquares()
+        {
+            for (int y = 0; y < 300; y += 30)
+            {
+                for (int x = 0; x < 300; x += 30)
+                {
+                    graphics.DrawRectangle(pen, x + 3, y + 3, 24, 24);
+                    Sleep(10);
+                    for (int xx = 6; xx < 24; xx += 6)
+                    {
+                        for (int yy = 6; yy < 24; yy += 6)
+                        {
+                            graphics.DrawEllipse(pen, x + xx, y + yy, 6, 6);
+                        }
+                    }
+                }
+            }
+            Refresh();
+        }
+
+        private void buttonLines_Click(object sender, EventArgs e)
+        {
+            HorizontalLines();
+            VerticallLines();
+        }
+
+        private void buttonAllLines_Click(object sender, EventArgs e)
+        {
+            AllLines();
+        }
+
+        private void buttonDiagLines_Click(object sender, EventArgs e)
+        {
+            DiagLines();
+        }
+
+        private void buttonConeOfRays_Click(object sender, EventArgs e)
+        {
+            ConeOfRays();
+        }
+
+        private void buttonConeOfRays1_Click(object sender, EventArgs e)
+        {
+            ConeOfRays1();
+        }
+
+        private void buttonSquares_Click(object sender, EventArgs e)
+        {
+            Squares();
+        }
+
+        private void buttonSquares1_Click(object sender, EventArgs e)
+        {
+            Squares1();
+        }
+
+        private void buttonTracery_Click(object sender, EventArgs e)
+        {
+            CreateTracery();
+        }
+
+        private void buttonTracery1_Click(object sender, EventArgs e)
+        {
+            CreateTracery1();
+        }
+
+        private void buttonLinesInSquares_Click(object sender, EventArgs e)
+        {
+            LinesInSquares();
+        }
+
+        private void buttonCirclesInSquares_Click(object sender, EventArgs e)
+        {
+            CirclesInSquares();
+        }
+    }
+}
